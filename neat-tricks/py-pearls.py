282333efa8bf32ae4d9e3450f8a7a3a434094215
Ankur Deshwal <a.s.deshwal@gmail.com>
2020-07-03 17:03:52 +0530

    Update py-pearls.py

--- neat-tricks/py-pearls.py
+++ neat-tricks/py-pearls.py
@@ -1,6 +1,6 @@
 # Awesome python shorthand tricks
 
 * Apply a function to multiple objects and assign to new objects
-```py
+```python
 x_train, y_train, x_valid, y_valid = map(torch.tensor, (x_train, y_train, x_valid, y_valid))
 ```
